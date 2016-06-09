a0df4b8aaca754df336259dab4b71bf244d8a23d
Akifumi Fukaya <akiiisuke@gmail.com>
2016-06-10 00:22:07 +0900

    selectable buttons using ImageButton & ToggleBbutton

--- app/src/main/java/net/akifumifukaya/togglebuttonsample/MainActivity.java
+++ app/src/main/java/net/akifumifukaya/togglebuttonsample/MainActivity.java
@@ -1,7 +1,10 @@
 package net.akifumifukaya.togglebuttonsample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageButton imageButton = (ImageButton) findViewById(R.id.imageButton);
+        if (imageButton != null) {
+            imageButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    imageButton.setSelected(!imageButton.isSelected());
+                }
+            });
+        }
+
+        final ToggleButton toggleButton = (ToggleButton) findViewById(R.id.toggleButton);
+        if (toggleButton != null) {
+            toggleButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    toggleButton.setSelected(!toggleButton.isSelected());
+                }
+            });
+        }
     }
 }
